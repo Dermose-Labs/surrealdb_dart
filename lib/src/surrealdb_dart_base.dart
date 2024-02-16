cf497286e07c47512ed9c42d294f0da25ecb65d7
Bibaswan Bhawal <bibaswan3600@gmail.com>
2024-02-16 14:32:45 -0800

    workaround for flutter web issues

--- lib/src/surrealdb_dart_base.dart
+++ lib/src/surrealdb_dart_base.dart
@@ -1,5 +1,3 @@
-// ignore: library_prefixes
-import 'package:dio/dio.dart';
 import 'package:surrealdb/surrealdb.dart' as internal;
 
 class SurrealDB {
@@ -15,8 +13,6 @@ class SurrealDB {
   void use({required String namespace, required String database}) {
     ns = namespace;
     db = database;
-
-    print('Connecting to namespace: $namespace, database: $database');
   }
 
   void authenticate(String token) {
@@ -42,45 +38,11 @@ class SurrealDB {
   }
 
   Future<dynamic> query(String query, [Map<String, dynamic> params = const {}]) async {
-    final DateTime start = DateTime.now();
-
     final internal.SurrealDB client = await _intializeClient();
-
-    final DateTime afterConnect = DateTime.now();
 
     final response = await client.query(query, params);
 
-    final DateTime afterQuery = DateTime.now();
-
-    print('Connect took: ${afterConnect.difference(start).inMilliseconds}ms');
-    print('Query took: ${afterQuery.difference(afterConnect).inMilliseconds}ms');
-
     client.close();
-
-    final DateTime end = DateTime.now();
-
-    print('Close took: ${end.difference(afterQuery).inMilliseconds}ms');
-    print('Total took: ${end.difference(start).inMilliseconds}ms');
-
-    return response;
-  }
-
-  Future<dynamic> queryHttp(String query) async {
-    final DateTime start = DateTime.now();
-
-    final client = Dio();
-
-    client.options.baseUrl = baseUrl;
-    client.options.headers['Accept'] = 'application/json';
-    client.options.headers['Authorization'] = autherizationToken;
-    client.options.headers['NS'] = ns;
-    client.options.headers['DB'] = db;
-
-    final response = await client.post('/sql', data: query);
-
-    final DateTime end = DateTime.now();
-
-    print('Total took: ${end.difference(start).inMilliseconds}ms');
 
     return response;
   }
